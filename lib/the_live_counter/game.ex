5174d8514f9d5f4ca3b831fb45df846de924a277
Jose Juan Reyes <neodevelop@gmail.com>
2020-08-16 22:05:08 -0500

    Implements Agent behaviour in Game

--- lib/the_live_counter/game.ex
+++ lib/the_live_counter/game.ex
@@ -1,12 +1,23 @@
 defmodule TheLiveCounter.Game do
   alias __MODULE__
+  use Agent, restart: :temporary
   defstruct counter: 0, id: 0
 
-  def create() do
+  def start_link(_opts) do
+    Agent.start_link(fn -> create() end)
+  end
+
+  defp create() do
     %Game{id: System.unique_integer([:positive])}
   end
 
-  def increase(%Game{counter: counter} = game) do
-    %Game{game | counter: counter + 1}
+  def get_counter(pid_game) do
+    Agent.get(pid_game, fn %Game{counter: counter} -> counter end)
+  end
+
+  def increase_counter(pid_game) do
+    Agent.update(pid_game, fn %Game{counter: counter} = game ->
+      %Game{game | counter: counter + 1}
+    end)
   end
 end
